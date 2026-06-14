fe1c8543b35b2e407aaaf354a8b8b86fe96398ee
guoliang <guol875@tydic.com>
2026-06-15 00:18:19 +0800

    fix: 管理后台入口 window 对象在模板中不可用
    将 window.location.href 改为 goAdmin() 方法调用

--- campus-platform/frontend/src/components/Layout.vue
+++ campus-platform/frontend/src/components/Layout.vue
@@ -65,7 +65,7 @@
               <el-dropdown-item @click="router.push('/my-reports')">我的举报</el-dropdown-item>
               <el-dropdown-item @click="router.push('/my-disputes')">我的纠纷</el-dropdown-item>
               <el-dropdown-item @click="router.push('/address')">收货地址</el-dropdown-item>
-              <el-dropdown-item @click="window.location.href='/admin'" v-if="userStore.userInfo.role === 1">管理后台</el-dropdown-item>
+              <el-dropdown-item @click="goAdmin" v-if="userStore.userInfo.role === 1">管理后台</el-dropdown-item>
               <el-dropdown-item divided @click="handleLogout">退出登录</el-dropdown-item>
             </el-dropdown-menu>
           </template>
@@ -125,6 +125,10 @@ async function loadNotifCount() {
 onUnmounted(() => {
   if (ws) ws.close()
 })
+
+function goAdmin() {
+  window.location.href = '/admin'
+}
 
 async function handleLogout() {
   await userStore.logout()
